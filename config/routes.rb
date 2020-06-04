1013ec010942151c18edb149ac3e7b994c9b82bb
ChunkySquirrel <matejmobile516@gmail.com>
2020-06-04 09:45:26 +0200

    jdsfasdf

--- config/routes.rb
+++ config/routes.rb
@@ -10,14 +10,14 @@ Rails.application.routes.draw do
   resources :sessions
   resources :tasks do
   resources :notes
-
+  end
   resources :reminders
+
   # get tasks/6/notes/2
 
 
   root to: 'home#index'
   get "about" => 'home#about'
-  resources :tasks
-  root to: 'tasks#index'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
