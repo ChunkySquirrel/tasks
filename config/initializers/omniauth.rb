6ab2bf20bea895e6ecc5dfe5ca676978b35bb678
ChunkySquirrel <matejmobile516@gmail.com>
2020-06-07 18:56:12 +0200

    keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
+  :client_id   => ENV['4e158d9388c49b40ce57'],
+  :client_secret => ENV['66f65506bbd6c3db6a0a9df2fa1a7396f05a8ad0']
   provider :github, Rails.application.credentials.github[:client_id], Rails.application.credentials.github[:client_secret]
-  
+
 end
